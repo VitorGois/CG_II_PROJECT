7e395dd9a20498be76264692d3293ab48f376a2c
Vitor Joaquim de Carvalho Gois <vitorcgois777@gmail.com>
2022-03-31 16:41:12 -0300

    feat: add hazard prefabs and game manager

---
+++ CG_II_PROJECT/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject hazardPrefab;
+    public int maxHazardToSpawn = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnHazards());        
+    }
+
+    private IEnumerator SpawnHazards()
+    {
+        var hazardToSpawn = Random.Range(1, maxHazardToSpawn);
+
+        for (int i = 0; i < hazardToSpawn; i++) 
+        {
+            var x = Random.Range(-7, 7);
+            var drag = Random.Range(0f, 2f);
+            var hazard = Instantiate(hazardPrefab, new Vector3(x, 11, 0), Quaternion.identity);
+            hazard.GetComponent<Rigidbody>().drag = drag;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        yield return SpawnHazards();
+    }
+
+}
